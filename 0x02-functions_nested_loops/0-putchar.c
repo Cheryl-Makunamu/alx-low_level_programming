885cb58385351f1cc649ca5174e4bd714f0df08b
Cheryl-Makunamu <makunamucheryl@gmail.com>
2023-10-11 12:54:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * Description - functions
+ * Return: always 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
