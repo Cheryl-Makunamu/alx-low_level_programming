7fcd858dd983a96212e89055564524cdd6c89313
Cheryl-Makunamu <makunamucheryl@gmail.com>
2023-10-12 13:04:51 -0700

    print

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multipies
+ * @a: checks parameter
+ * @b: checks parameter
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints 0 through 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+}
