91a5a8dec06151937556636bf3a966584d430e57
Cheryl-Makunamu <makunamucheryl@gmail.com>
2023-10-23 12:17:55 -0700

    memcpy,strchr,strspn

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: input value
+ * @c: input value
+ * Return: 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int k = 0;
+
+	for (; s[ik] >= '\0' ; k++)
+	{
+		if (s[k] == c)
+			return (&s[k]);
+	}
+	return (0);
+}
