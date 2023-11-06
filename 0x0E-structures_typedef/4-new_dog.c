4afafb558c932c79b552d5fe10d1943a261dac90
Cheryl-Makunamu <makunamucheryl@gmail.com>
2023-11-06 13:17:17 -0800

    chnges

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ int _strlen(char *s)
 }
 
 /**
- * strcopy - copies the string
+ * _strcopy - copies the string
  * @src : source of the string
  * @dest: buffer that stores the string
  * Return: dest pointer
@@ -28,38 +28,47 @@ char*_strcopy(char *dest, char *src)
 	int index = 0;
 
 
-	for (index = 0 ; src[index] ;index ++;)
+	for (index = 0 ; src[index] ; index ++)
 		dest[index] = src[index];
 	dest[index] = '\0';
 	return (dest);
 }
 
 /**
- * strcopy - copies the string
- * @src : source of the string
- * @dest: buffer that stores the string
- * Return: dest pointer
+ * new_dog - creates new dog 
+ * @age: dogs age
+ * @name: dogs name
+ * @owner: the dogd owner
+ * Return: null
  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+        
+        if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
-        dog_t *dog;
-        int l1, l2;
-
-        l1 = _strlen(name);
-        l2 = _strlen(owner);
         dog = malloc(sizeof(dog_t));
+	
         if (dog == NULL)
-                return (NULL);
-        dog->name =malloc(sizeof(char) * (l1 + 1));
-        if (dog->name == NULL)
-        {
+		return (NULL);
+	
+	dog->name= malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dog->owner == NULL)
+        
+	{
                 free(dog);
-                return (NULL);
+		return (NULL);
 	}
-	dog-> = malloc(sizeof(char) * (l2 + 1));
+	dog->owner= malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
-		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
+	return (dog);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - that frees dog's memory
