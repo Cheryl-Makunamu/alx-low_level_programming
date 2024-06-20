1873a50f9dc472c8fc865179955ae040a24ec9e8
Cheryl-Makunamu <makunamucheryl@gmail.com>
2024-06-20 13:29:30 -0700

    nintel

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main point
+ * Description - prints letters
+ * Return: zero if true
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main point
+ * Description - prints words
+ * Return: to be 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
